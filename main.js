820c450576c3a8487131bca9e327c39ffe20d921
”Louise <loui82m9@stud.kea.dk>
2021-12-13 14:11:51 +0100

    Louise mandag footer og javascript

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 
 const btn = document.querySelector(".toggle-btn");
 const nav = document.querySelector("nav");
+
 function toggleMenu() {
   console.log("toggleMenu");
   nav.classList.toggle("shown");
+
   const menuShown = nav.classList.contains("shown");
+
   if (menuShown) {
     btn.textContent = "Luk";
   } else {
     btn.textContent = "Menu";
   }
 }
+
 btn.addEventListener("click", toggleMenu);
