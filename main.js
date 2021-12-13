8eb8799e571780cbc39464fd0f2e7d5409c04867
Your Name <youremail@yourdomain.comAndreaandr52j9@stud.kea.dk>
2021-12-13 12:48:06 +0100

    burger menu

---
+++ main.js
@@ -0,0 +1,15 @@
+//burgermenu
+
+const btn = document.querySelector(".toggle-btn");
+const nav = document.querySelector("nav");
+function toggleMenu() {
+  console.log("toggleMenu");
+  nav.classList.toggle("shown");
+  const menuShown = nav.classList.contains("shown");
+  if (menuShown) {
+    btn.textContent = "Luk";
+  } else {
+    btn.textContent = "Menu";
+  }
+}
+btn.addEventListener("click", toggleMenu);
